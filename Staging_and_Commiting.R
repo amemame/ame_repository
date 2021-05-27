26d6451fee053ec22b1fef9a96c845adb665bf05
„amemame“ <ame.wbr@gmail.com>
2021-05-27 08:26:31 +0000

    staging and commiting how to

---
+++ Staging_and_Commiting.R
@@ -0,0 +1,5 @@
+#Stageing and commiting 
+#git config --global user.email „YOUR EMAIL “ <- add email of git account
+#git config --global user.name „YOUR NAME“ <- and add username
+#add the files you want to stage to Git by marking in the "Git" to select the files to stage
+#then commit the files to add them to the Git repository by pushing them (green arrow) 
